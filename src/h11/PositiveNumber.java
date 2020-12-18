41be5b9be64e0f8e5b60bfe2d85f0bc9f34b13f5
niklasdiekhoner <niklasdiekhoener@gmail.com>
2020-12-18 20:50:53 +0100

    h11 fertig

--- src/h11/PositiveNumber.java
+++ src/h11/PositiveNumber.java
@@ -1,9 +1,16 @@
 package h11;
 
+/**
+ * Diese Klasse kann Zahlen aus dem Binaer-, Dezimal- und Hexadezimalsystem erhalten
+ * und diese in eins der anderen umrechnen und zurueckgeben.
+ */
 public class PositiveNumber {
     private final String hexDigits = "0123456789ABCDEF";
     private int value; // es wird immer die Dezimalzahl gespeichert
 
+    /**
+     * Konstruktor der ein neues PositiveNumber Objekt erzeugt.
+     */
     public PositiveNumber() {
         this.value = 0;
     }
@@ -19,10 +26,20 @@ public class PositiveNumber {
         System.out.println("Hexadezimal: " + zs.getHexadecimal());
     }
 
+    /**
+     * Gibt die Zahl als Dezimalzahl zurueck.
+     *
+     * @return Dezimalzahl als String
+     */
     public String getDecimal() {
         return "" + value;
     }
 
+    /**
+     * Setzt die Zahl aus der Dezimalzahl.
+     *
+     * @param s Dezimalzahl als String
+     */
     public void setDecimal(String s) {
         this.value = 0;
         proof(s);
@@ -33,15 +50,25 @@ public class PositiveNumber {
         }
     }
 
+    /**
+     * Gibt die Zahl als Hexadezimalzahl zurueck.
+     *
+     * @return Hexadezimalzahl als String
+     */
     public String getHexadecimal() {
         String binary = decimalToBinary(value);
         StringBuilder res = new StringBuilder();
         for (int i = binary.length() - 4; i >= 0; i -= 4) {
-            res.insert(0,hexDigits.charAt(binaryToDecimal(binary.substring(i, i + 4))));
+            res.insert(0, hexDigits.charAt(binaryToDecimal(binary.substring(i, i + 4))));
         }
         return res.toString();
     }
 
+    /**
+     * Setzt die Zahl aus der Hexadezimalzahl.
+     *
+     * @param s Hexadezimalzahl als String
+     */
     public void setHexadecimal(String s) {
         this.value = 0;
         s = s.toUpperCase();
@@ -53,31 +80,50 @@ public class PositiveNumber {
         }
     }
 
+    /**
+     * Gibt die Zahl als Binaerzahl zurueck.
+     *
+     * @return Binaerzahl als String
+     */
     public String getBinary() {
         return this.decimalToBinary(value);
     }
 
+    /**
+     * Setzt die Zahl aus der Binaerzahl.
+     *
+     * @param s Binaerzahl als String
+     */
     public void setBinary(String s) {
         this.value = 0;
         proof(s);
-        for (int i = 0; i < s.length(); i++) {
-            value += (s.charAt(i) - 48) * Math.pow(10, s.length() - 1 - i);
-        }
-        value = this.binaryToDecimal("" + value);
+        value = this.binaryToDecimal(s);
     }
 
+    /**
+     * Hilfsmethode die von dezimal in binaer umrechnet.
+     *
+     * @param x Dezimalzahl als int
+     * @return Binaerzahl als String
+     */
     private String decimalToBinary(int x) {
         StringBuilder res = new StringBuilder();
         while (x > 0) {
             res.insert(0, x % 2 == 0 ? "0" : "1");
             x /= 2;
         }
-        while(res.length() % 4 != 0){
-            res.insert(0,0);
+        while (res.length() % 4 != 0) {
+            res.insert(0, 0);
         }
         return res.toString();
     }
 
+    /**
+     * Hilfsmethode die von binear in dezimal umrechnet.
+     *
+     * @param s Binaerzahl als String
+     * @return Dezimalzahl als int
+     */
     private int binaryToDecimal(String s) {
         int res = 0;
         for (int i = 0; i <= s.length() - 1; i++) {
@@ -86,6 +132,11 @@ public class PositiveNumber {
         return res;
     }
 
+    /**
+     * Hilfsmethode die prueft ob eine Zahl die eingegeben wurde negativ ist.
+     *
+     * @param s Zahl als String
+     */
     private void proof(String s) {
         if (s.charAt(0) == '-') {
             throw new NumberFormatException("Nur positive Zahlen");
